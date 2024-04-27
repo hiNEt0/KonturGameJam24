a889343d7219211871b80d671a1f18bd31f2da99
Tarakanoed <svitch1337@gmail.com>
2024-04-28 01:52:29 +0500

    event manager base

--- fire_ball.gd
+++ fire_ball.gd
@@ -5,7 +5,7 @@ var direction = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(direction)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
