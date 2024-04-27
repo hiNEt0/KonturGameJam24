147400cca1ce81bfd5192c55d9e08555480d9e41
Tarakanoed <svitch1337@gmail.com>
2024-04-27 21:02:08 +0500

    auto 0.1 fixed

--- FireBaller.gd
+++ FireBaller.gd
@@ -13,11 +13,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if timer < 0:
-		print(get_parent().position)
 		timer = cooldown
 		var current_missile = missile.instantiate()
 		current_missile.position = get_parent().position
-		current_missile.target = get_target()
+		current_missile.direction = (Vector2(10, 0) - get_parent().position).normalized()
 		get_tree().get_root().add_child(current_missile)
 	else:
 		timer -= delta
@@ -29,4 +28,4 @@ func get_target_enemy():
 	pass
 	
 func get_target():
-	return Vector2(0, 0)
+	return Vector2(100, 0)
--- fire_ball.gd
+++ fire_ball.gd
@@ -1,12 +1,14 @@
 extends Area2D
 @export var target = Vector2(0,0)
 @export var speed = 100
+var direction = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print(direction)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position = position.move_toward((target - self.position).normalized(), speed * delta)
+	position += direction * speed * delta
+	
