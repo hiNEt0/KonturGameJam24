1411ebe341cbb5dfeba74eae96a25474f2c885d5
hiNEt0 <hinet.f23@gmail.com>
2024-04-28 04:29:11 +0500

    dash on space added

--- player_2_movement.gd
+++ player_2_movement.gd
@@ -4,11 +4,14 @@ extends CharacterBody2D
 @export var speed = 100 # How fast the player will move (pixels/sec).
 @export var hp = 100
 @export var damage_cooldown = 1.0
+@export var dash_cooldown = 1.0
+@export var dash_force = 30
 @onready var aim = $aim
 @onready var sprite = $sprites
 @onready var direction = Vector2(1, 0)
 @onready var last_direction = direction
 var movement_modifier = Vector2(1, 1)
+var dash = 1
 
 func get_damage(damage):
 	if damage_cooldown >= 1.0:
@@ -38,6 +41,14 @@ func _physics_process(delta):
 	velocity = Vector2.ZERO # The player's movement vector.
 	if damage_cooldown < 1.0:
 		damage_cooldown += delta
+	if dash_cooldown < 1.0:
+		dash_cooldown += delta
+		
+	if Input.is_key_pressed(KEY_SPACE) and dash_cooldown >= 1.0:
+		dash = dash_force
+		dash_cooldown = 0.0
+	else:
+		dash = 1
 		
 	if Input.is_action_pressed("move_right"):
 		sprite.play("wiz_right")
@@ -65,7 +76,7 @@ func _physics_process(delta):
 	direction = get_direction()
 	velocity.x *= movement_modifier.x
 	velocity.y *= movement_modifier.y
-	move_and_collide(velocity.normalized() * speed * delta)
+	move_and_collide(velocity.normalized() * speed * delta * dash)
 	
 func get_direction():
 	var directing_velocity = velocity
